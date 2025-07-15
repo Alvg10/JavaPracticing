ec6d027df712e7625f60fbfeca7a11ee5bad6fba
Dev.Eg4 <114100372+Alvg10@users.noreply.github.com>
2025-07-14 20:05:14 -0500

    Practice of Anonimo Class

--- src/App.java
+++ src/App.java
@@ -1,46 +1,20 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+
 
 public class App {
     
     public static void main(String[] args) throws Exception {
        
-    System.out.println("Hola");
-    Map<String, Double> superHeroes = new HashMap<String, Double>();
-    superHeroes.put("Banana", 5.40);        
-    superHeroes.put("Durazno", 3.20);
-    superHeroes.put("Manzana", 2.50);        
-    superHeroes.put("pera", 4.30);        
-    superHeroes.put("papaya", 6.80);        
-    superHeroes.put("Granadilla", 7.20);        
-    superHeroes.put("Piña", 8.76);        
-    superHeroes.put("Zandialla", 9.40);        
+        Animal ani = new Animal () {
 
-    for ( String fruta : superHeroes.keySet()) {
-        System.out.println("La fruta " + fruta + " cuesta " + superHeroes.get(fruta));
+            @Override
+            void hacerSonido() {
+                System.out.println("El animal hace un sonido");
+            }
+        };
+
+        ani.hacerSonido();
+
+
     }
 
-    List<String> Automoviles = new ArrayList<String>();
-    Automoviles.add("Toyota");
-    Automoviles.add("Nissan");  
-    Automoviles.add("Chevrolet");
-    Automoviles.add("Kia");
-
-for (String auto : Automoviles) {
-    System.out.println("El auto " + auto + " prueba exitosa");
 }
-
-Caja<String> cajaDeTexto = new Caja<>("Hola mundo");
-System.out.println("Caja contiene: " + cajaDeTexto.getContenido());
-
-Caja<Integer> cajaDeNumero = new Caja<>(123);
-System.out.println("Caja contiene: " + cajaDeNumero.getContenido());
-
-Caja<String> cajaDeTexto1 = new Caja<>("Prueba01");
-cajaDeTexto1.setContenido("Prueba02");
-System.out.println("Caja contiene: " + cajaDeTexto1.getContenido());
-
-
-}}  
